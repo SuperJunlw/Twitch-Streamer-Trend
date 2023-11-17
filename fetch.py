9b825dbbb5fb9afa530758bf475f0ad9d13aa1d6
RoyCDev <rcheung0117@gmail.com>
2023-11-16 16:11:40 -0800

    fetch code v2.0 + new 11/15 data

--- fetch.py
+++ fetch.py
@@ -5,49 +5,55 @@ from pprint import pprint as pp
 from datetime import date
 
 """
-    get the list of top 100 streamers in the past 7 days from StreamCharts 
+    get the list of top 100 streamers (and their data) in the past 7 days from StreamCharts 
     Client ID and Token needed
 """
 
-def get_top_100_streamers():
+def get_top_100_streamers_data():
     res = requests.get(' https://streamscharts.com/api/jazz/channels?platform=twitch&time=7-days', headers={
         "Client-ID": "Jfwu27gO2lFSwE6E",
         "Token": "$2y$10$m.jYZ6FNiPauWKFZAZPKeeFz0JVVVWY1UCMA39thMal8jmKPn52lu"
     })
 
-    res = res.json()["data"]
-    # pp(res)
+    # pp(res.json()["data"])
+    return res.json()["data"]
 
-    streamers = [None]*100
-    for i, streamer in enumerate(res):
-        streamers[i] = streamer["channel_name"]
-
-    return streamers
 
 """
-    Get the top #X+1 - #Y streamers' stats in the past 30 days from TwitchTracker
-        ex. fetch_streamers_data(0, 100) = get the top #1 - #100 stats
-
-    Please only fetch a few each time. API request won't work if you overburden the provider 
+    store streamer's data to flat files. repeat for every streamer in the top 100 list
+    data (including the list) is gathered from StreamChart
 """
 
-def fetch_top_streamers_data(x,y):
-    streamers_folder_dir = os.path.join(os.getcwd(), "streamers")
+def write_top_100_streamers_data():
+    streamers_data = get_top_100_streamers_data()
+
+    streamers_folder_dir = os.path.join(os.getcwd(), "streamers3")
     if not os.path.isdir(streamers_folder_dir):
         os.makedirs(streamers_folder_dir)
+    
+    extract = ['airtime_in_m', 'live_views', 'average_viewers', 'peak_viewers', 'hours_watched', 'followers_gain']
+    
+    for i, streamer in enumerate(streamers_data):
+        with open(os.path.join(streamers_folder_dir, f"{streamer['channel_name']}.txt"), "a") as f:
+            
+            extract_dict = {"day": str(date.today()), "rank": i+1}
+            for key in extract:
+                extract_dict[key] = streamer[key]
+            # extract_dict["followers_total"] = fetch_followers_total(streamer['channel_name'])
 
-    streamers = get_top_100_streamers()
-    for streamer in streamers[x:y]:
-        with open(os.path.join(streamers_folder_dir, f"{streamer}.txt"), "a") as f:
-            f.write(str(date.today()) + ",")
-
-            res = requests.get(f"https://twitchtracker.com/api/channels/summary/{streamer}")
-            f.write(res.content.decode())
+            f.write(str(extract_dict))
             f.write("\n")
 
 
-# Code to fetch 5 streamers from the top 100 every 30 sec
-# for i in range(20):
-#     fetch_top_streamers_data(5*i, 5*(i+1))
-#     time.sleep(30)
+"""
+    Please only fetch a few each time. API request won't work if you overburden the provider 
+"""
 
+# def fetch_followers_total(streamer):
+#     res = requests.get(f"https://twitchtracker.com/api/channels/summary/{streamer}")
+#     time.sleep(6)
+#     return res.json()["followers_total"]
+
+
+# Call this code everyday
+write_top_100_streamers_data()
