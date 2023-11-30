b930351ffcf86411ae83a0bb9573b4ab9a902bef
RoyCDev <rcheung0117@gmail.com>
2023-11-29 22:29:21 -0800

    landing page + more basic web logic

--- flask/app.py
+++ flask/app.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append('../CS122')
 import draw_chart as dc
 import prediction as pd
-
+import read_data as rd
 
 app = Flask(__name__)
 
@@ -19,19 +19,28 @@ def main():
     return render_template("main.html")
 
 
-@app.route("/forecast", methods=["GET"])
+@app.route("/current", methods=["GET"])
+def current_trend():
+    fig = dc.draw_bar_graph(rd.get_top10_popularity())
+    data = generate_plot(fig)
+    return render_template("current.html", img=f"<img src='data:image/png;base64,{data}'/>")
+
+
+@app.route("/forecast")
 def forecast():
-    months = request.args["months"]
-    data = generate_plot(int(months))
-    return render_template("main.html", img=f"<img src='data:image/png;base64,{data}'/>")
+    try:
+        months = int(request.args["months"])
+        fig = dc.draw_future_trends(pd.predict_popularity(months), months)
+        data = generate_plot(fig)
+        return render_template("current.html", img=f"<img src='data:image/png;base64,{data}'/>")
+    except:
+        return render_template("current.html")
 
 
 # Apparently it's not recommanded to use mathplotlib.pyplot in Flask
 # More details on https://matplotlib.org/stable/gallery/user_interfaces/web_application_server_sgskip.html
-def generate_plot(months):
-    fig = dc.draw_future_trends(pd.predict_popularity(months), months)
-
-    # This code came from the website
+# This code came from the website
+def generate_plot(fig):
     buf = BytesIO()
     fig.savefig(buf, format="png")
     return base64.b64encode(buf.getbuffer()).decode("ascii")
